b6d7c6d4945ef217179db3c855abf7f2014b36dd
brianach <>
2023-07-31 16:47:13 +0000

    content added to about page

--- about/models.py
+++ about/models.py
@@ -4,7 +4,7 @@ from cloudinary.models import CloudinaryField
 
 class About(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    profile_image = CloudinaryField('image', default='placeholder')
+    about_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
 
     def __str__(self):
