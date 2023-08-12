f3b7049e212f3d67d347770efcd632c4b6c14f7c
brianach <>
2023-08-12 10:59:40 +0000

    retest static settings

--- wells/settings.py
+++ wells/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = 'staticfiles'
 
